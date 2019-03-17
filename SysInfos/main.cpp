2d355c35af0b695e6b3a09d556deec111f4d30a4
lionel-git <lionel.desorme@free.fr>
2019-03-17 14:05:24 +0100

    Test hyper threading

--- SysInfos/main.cpp
+++ SysInfos/main.cpp
@@ -17,6 +17,9 @@
 
 #include <intrin.h>
 
+#include <iostream>
+#include <string>
+
 
 #pragma comment(lib, "user32.lib")
 
@@ -258,6 +261,68 @@ void test7()
 	std::cout << "IsVM: " << isVM << std::endl;
 }
 
+
+
+
+void cpuID(unsigned i, unsigned regs[4]) {
+#ifdef _WIN32
+	__cpuid((int *)regs, (int)i);
+
+#else
+	asm volatile
+		("cpuid" : "=a" (regs[0]), "=b" (regs[1]), "=c" (regs[2]), "=d" (regs[3])
+			: "a" (i), "c" (0));
+	// ECX is set to zero for CPUID function 4
+#endif
+}
+
+// Marche plus...
+void testHT()
+{
+	unsigned int regs[4];
+
+	// Get vendor
+	char vendor[12];
+	cpuID(0, regs);
+	((unsigned *)vendor)[0] = regs[1]; // EBX
+	((unsigned *)vendor)[1] = regs[3]; // EDX
+	((unsigned *)vendor)[2] = regs[2]; // ECX
+	std::string cpuVendor = std::string(vendor, 12);
+
+	// Get CPU features
+	cpuID(1, regs);
+	unsigned cpuFeatures = regs[3]; // EDX
+
+	// Logical core count per CPU
+	cpuID(1, regs);
+	unsigned logical = (regs[1] >> 16) & 0xff; // EBX[23:16]
+	std::cout << " logical cpus: " << logical << std::endl;
+	unsigned cores = logical;
+
+	if (cpuVendor == "GenuineIntel") {
+		// Get DCP cache info
+		cpuID(4, regs);
+		cores = ((regs[0] >> 26) & 0x3f) + 1; // EAX[31:26] + 1
+
+	}
+	else if (cpuVendor == "AuthenticAMD") {
+		// Get NC: Number of CPU cores - 1
+		cpuID(0x80000008, regs);
+		cores = ((unsigned)(regs[2] & 0xff)) + 1; // ECX[7:0] + 1
+	}
+
+	std::cout << "    cpu cores: " << cores << std::endl;
+
+	// Detect hyper-threads  
+	bool hyperThreads = cpuFeatures & (1 << 28) && cores < logical;
+
+	std::cout << "hyper-threads: " << (hyperThreads ? "true" : "false") << std::endl;
+}
+
+
+
+
+
 int main(int argc, char **argv)
 {
 	std::cout << "Hello world" << std::endl;
@@ -268,6 +333,7 @@ int main(int argc, char **argv)
 	test5();
 	test6();
 	test7();
+	testHT();
 	std::cout << "Wait key..." << std::endl;
 	int c=getchar();
 }
